68fa0642970376455ff72f043d90ac9758fc0f63
Matheus Oliveira <112398318+Mathreus@users.noreply.github.com>
2026-01-20 14:53:20 -0300

    Update AFO.01 - Saldo de adiantamento realizados a colaborador (aberto e fechado).sql

--- Adiantamentos a Fornecedores/AFO.01 - Saldo de adiantamento realizados a colaborador (aberto e fechado).sql
+++ Adiantamentos a Fornecedores/AFO.01 - Saldo de adiantamento realizados a colaborador (aberto e fechado).sql
@@ -1,8 +1,10 @@
-DECLARE @DATA_BASE DATE = '2025-12-31' -- Data Base
+-- Data Base
+DECLARE @DATA_BASE DATE = '2025-12-31' 
 
 SELECT
     COD_ESTABELECIMENTO,
     COD_FORNECEDOR,
+    MAX([CNPJ/CPF]) AS CNPJ_CPF,
     MAX(NOME_FORNECEDOR) AS NOME_FORNECEDOR,
     MAX(PERFIL_LANC) AS PERFIL_LANC,
     -- Soma condicional para o status 'ABERTO'
@@ -11,22 +13,24 @@ SELECT
     SUM(CASE WHEN STATUS = 'FECHADO' THEN VALOR_MOEDA ELSE 0 END) AS VALOR_FECHADO,
     -- Soma valor geral
     SUM(VALOR_MOEDA) AS VALOR_TOTAL,
-    -- Contagem por status
+    -- Quantidade de adiantamento em aberto 
     SUM(CASE WHEN STATUS = 'ABERTO' THEN 1 ELSE 0 END) AS QTD_ABERTO,
+    --Quantidade de adiantamento fechados
     SUM(CASE WHEN STATUS = 'FECHADO' THEN 1 ELSE 0 END) AS QTD_FECHADO,
-    -- Quantidade Total
+    -- Quantidade Total de adiantamentos
     (SUM(CASE WHEN STATUS = 'ABERTO' THEN 1 ELSE 0 END)) + (SUM(CASE WHEN STATUS = 'FECHADO' THEN 1 ELSE 0 END)) AS QTD_TOTAL
 FROM
     VW_AUDIT_RM_TRANSACOES_FORNECEDOR
 WHERE
     COD_ESTABELECIMENTO IN ('R351', 'R352') -- Alterar para os Distribuidores que serão auditados
-    AND DATA_TRANSACAO <= @DATA_BASE
-    AND (DATA_LIQUIDACAO >= DATEADD(DAY, 1, @DATA_BASE) OR DATA_LIQUIDACAO = '1900-01-01')
-    AND PERFIL_LANC = 'AFO'
-    AND STATUS IN ('ABERTO', 'FECHADO') -- Filtro baseado nos status usados nas somas
+    AND DATA_TRANSACAO <= @DATA_BASE -- Data Base
+    AND (DATA_LIQUIDACAO >= DATEADD(DAY, 1, @DATA_BASE) OR DATA_LIQUIDACAO = '1900-01-01') -- Filtro de liquidações que serão liquidadas e ainda não foram liquidadas
+    AND PERFIL_LANC = 'AFO' -- Filtro de adiantamento a fornecedores
+    AND STATUS IN ('ABERTO', 'FECHADO') -- Filtro de status aberto e fechado
 GROUP BY
+    COD_ESTABELECIMENTO,
     COD_FORNECEDOR
 HAVING
-    SUM(VALOR_MOEDA) <> 0
+    SUM(VALOR_MOEDA) <> 0 -- Retorna valores diferentes de zero
 ORDER BY
     SUM(CASE WHEN STATUS = 'ABERTO' THEN VALOR_MOEDA ELSE 0 END) DESC
